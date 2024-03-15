5104988625c1e64d16ea6c19c38dee44c009e304
leeseongchi <kyoti0414@naver.com>
2024-03-15 17:50:07 +0900

    240315 Library_Project

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,11 +1,41 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { REGEX } from "../constants/regex";
 
-export const useInput = () => {
+export const useInput = (property) => {
     const [ value, setValue ] = useState("");
+    const [ message, setMessage ] = useState(null);
+
+    useEffect(() => {
+        if(!value) {
+            setMessage(() => null);
+            return;
+        }
+
+        const regexEntries = Object.entries(REGEX);
+        for(let [ k, v ] of regexEntries) {
+            if(property === k) {
+                if(v.regexr.test(value)) {
+                    setMessage(() => {
+                        return {
+                            type: "success",
+                            text: ""
+                        }
+                    })
+                }else {
+                    setMessage(() => {
+                        return {
+                            type: "error",
+                            text: v.text
+                        }
+                    })
+                }
+            }
+        }
+    }, [value])
 
     const handleonChange = (e) => {
         setValue(() => e.target.value);
     }
     
-    return [ value, setValue, handleonChange ];
+    return [ value, handleonChange, message, setValue, setMessage ];
 }
