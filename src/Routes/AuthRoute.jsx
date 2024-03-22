8150c5013b1bab674734af40c906daf91a09f4fe
leeseongchi <kyoti0414@naver.com>
2024-03-22 17:46:30 +0900

    240322 Library_Project

--- src/Routes/AuthRoute.jsx
+++ src/Routes/AuthRoute.jsx
@@ -12,6 +12,7 @@ import { GridLoader } from 'react-spinners';
 import FullSizeLoader from '../components/FullSizeLoader/FullSizeLoader';
 import MyPage from '../pages/MyPage/MyPage';
 import PageContainer from '../components/PageContainer/PageContainer';
+import PasswordEditPage from '../pages/PasswordEditPage/PasswordEditPage';
 
 // useQuery => GET 요청시에 사용
 // 첫번 째 매개변수 => 배열 ["key값", dependency]
@@ -53,6 +54,7 @@ function AuthRoute(props) {
                         <Route path="/auth/*" element={ <AuthPage /> } />
                         <Route path="/" element={ <HomePage /> } />
                         <Route path="/account/mypage" element={ <MyPage /> } />
+                        <Route path="/account/edit/password" element={ <PasswordEditPage /> } />
                     </Routes>
                 }
             </PageContainer>
